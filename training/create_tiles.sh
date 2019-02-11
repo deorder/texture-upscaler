bdae21e988f33cc852b66e737691915ff02f1c16
Deorder <31899960+deorder@users.noreply.github.com>
2019-02-12 00:38:49 +0100

    Fixed division by zero

--- training/create_tiles.sh
+++ training/create_tiles.sh
@@ -24,40 +24,46 @@ find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \) | while read FILENA
   ESCAPED_FILE=$(printf '%q' "${FILENAME}")
   DIRNAME_HASH=$(echo ${DIRNAME} | md5sum | cut -d' ' -f1)
 
-  COLOR_TYPE=$(identify -format '%[channels]' "${FILENAME}")
-  IMAGE_WIDTH=$(identify -format '%[width]' "${FILENAME}")
-  IMAGE_HEIGHT=$(identify -format '%[height]' "${FILENAME}")
+  if [ ! -f "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_000.png" ]; then
 
-  RELATIVE_DIR=$(realpath --relative-to "${INPUT_DIR}" "${DIRNAME}")
+    IMAGE_WIDTH=$(identify -format '%[width]' "${FILENAME}")
+    IMAGE_HEIGHT=$(identify -format '%[height]' "${FILENAME}")
+    COLOR_TYPE=$(identify -format '%[channels]' "${FILENAME}")
 
-  if [ "$((${IMAGE_WIDTH}))" -ge "${MIN_TILE_WIDTH}" ] && [ "$((${IMAGE_HEIGHT}))" -ge "${MIN_TILE_HEIGHT}" ]; then
+    RELATIVE_DIR=$(realpath --relative-to "${INPUT_DIR}" "${DIRNAME}")
 
-    VERTICAL_SUBDIVISIONS=$((${IMAGE_HEIGHT} / ${MAX_TILE_HEIGHT}))
-    if [ "$((${IMAGE_HEIGHT} / ${VERTICAL_SUBDIVISIONS}))" -le "${MIN_TILE_HEIGHT}" ]; then
-      VERTICAL_SUBDIVISIONS=$((${IMAGE_HEIGHT} / ${MIN_TILE_HEIGHT}))
-    fi
-    HORIZONTAL_SUBDIVISIONS=$((${IMAGE_WIDTH} / ${MAX_TILE_WIDTH}))
-    if [ "$((${IMAGE_WIDTH} / ${HORIZONTAL_SUBDIVISIONS}))" -le "${MIN_TILE_WIDTH}" ]; then
-      HORIZONTAL_SUBDIVISIONS=$((${IMAGE_WIDTH} / ${MIN_TILE_WIDTH}))
-    fi
+    if [ "$((${IMAGE_WIDTH}))" -ge "${MIN_TILE_WIDTH}" ] && [ "$((${IMAGE_HEIGHT}))" -ge "${MIN_TILE_HEIGHT}" ]; then
 
-    if [ "$(convert "${FILENAME}" -alpha off -format "%[k]" info:)" -gt "1" ]; then
-      echo ${FILENAME}, rgb \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
-      convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_%03d.png"
-    else
-      echo ${FILENAME}, rgb single color, skipped
-    fi
-    if [ "${COLOR_TYPE}" == "rgba" ] || [ "${COLOR_TYPE}" == "srgba" ]; then
-      if [ "$(convert "${FILENAME}" -alpha extract -format "%[k]" info:)" -gt "1" ]; then
-        echo ${FILENAME}, alpha \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
-        convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png"
-      else
-        echo ${FILENAME}, alpha single color, skipped
+      VERTICAL_SUBDIVISIONS=$((${IMAGE_HEIGHT} / ${MAX_TILE_HEIGHT}))
+      if [ "${VERTICAL_SUBDIVISIONS}" -lt "1" ]; then
+        VERTICAL_SUBDIVISIONS=$((${IMAGE_HEIGHT} / ${MIN_TILE_HEIGHT}))
       fi
+      HORIZONTAL_SUBDIVISIONS=$((${IMAGE_WIDTH} / ${MAX_TILE_WIDTH}))
+      if [ "${HORIZONTAL_SUBDIVISIONS}" -lt "1" ]; then
+        HORIZONTAL_SUBDIVISIONS=$((${IMAGE_WIDTH} / ${MIN_TILE_WIDTH}))
+      fi
+
+      if [ "$(convert "${FILENAME}" -alpha off -format "%[k]" info:)" -gt "1" ]; then
+        echo ${FILENAME}, rgb \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
+        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_%03d.png"
+      else
+        echo ${FILENAME}, rgb single color, skipped
+      fi
+      if [ "${COLOR_TYPE}" == "rgba" ] || [ "${COLOR_TYPE}" == "srgba" ]; then
+        if [ "$(convert "${FILENAME}" -alpha extract -format "%[k]" info:)" -gt "1" ]; then
+          echo ${FILENAME}, alpha \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
+          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png"
+        else
+          echo ${FILENAME}, alpha single color, skipped
+        fi
+      fi
+
+    else
+      echo ${FILENAME} too small \(${IMAGE_WIDTH}x${IMAGE_HEIGHT}\), skipped
     fi
 
   else
-    echo ${FILENAME} too small \(${IMAGE_WIDTH}x${IMAGE_HEIGHT}\), skipped
+    echo ${FILENAME}, already processed, skipped
   fi
-  
+
 done
