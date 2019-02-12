9be50c1a7729a5d86f9b3c3efad9f991e4946910
Deorder <31899960+deorder@users.noreply.github.com>
2019-02-12 02:31:44 +0100

    Added support for multiple jobs

--- training/create_tiles.sh
+++ training/create_tiles.sh
@@ -23,8 +23,18 @@ HR_FILTER=point
 HR_INTERPOLATE=Nearest
 HR_OUTPUT_DIR=./output.hr
 
-mkdir -p "${LR_OUTPUT_DIR}"
-mkdir -p "${HR_OUTPUT_DIR}"
+# Category regexp for Skyrim SE
+CATEGORY_REGEXP='s/.*_\(a\|b\|d\|e\|g\|h\|m\|n\|p\|s\|an\|bl\|em\|sk\|msn\|rim\)$/\1/ip'
+
+wait_for_jobs() {
+  local JOBLIST=($(jobs -p))
+  if [ "${#JOBLIST[@]}" -gt "4" ]; then
+    for JOB in ${JOBLIST}; do
+      echo Waiting for job ${JOB}...
+      wait ${JOB}
+    done
+  fi
+}
 
 find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \) | while read FILENAME; do
 
@@ -33,6 +43,7 @@ find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \) | while read FILENA
   ESCAPED_DIR=$(printf '%q' "${DIRNAME}")
   ESCAPED_FILE=$(printf '%q' "${FILENAME}")
   DIRNAME_HASH=$(echo ${DIRNAME} | md5sum | cut -d' ' -f1)
+  CATEGORY=$(echo ${BASENAME} | sed -ne "${CATEGORY_REGEXP}")
 
   if [ ! -f "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_000.png" ]; then
 
@@ -54,17 +65,25 @@ find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \) | while read FILENA
       fi
 
       if [ "$(convert "${FILENAME}" -alpha off -format "%[k]" info:)" -gt "1" ]; then
-        echo ${FILENAME}, rgb \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
-        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_%03d.png"
-        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_%03d.png"
+        mkdir -p "${HR_OUTPUT_DIR}/${CATEGORY}/rgb"
+        mkdir -p "${LR_OUTPUT_DIR}/${CATEGORY}/rgb"
+        echo ${FILENAME}, rgb \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\) ${CATEGORY}
+        wait_for_jobs
+        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${CATEGORY}/rgb/${DIRNAME_HASH}_${BASENAME}_%03d.png" &
+        wait_for_jobs
+        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${CATEGORY}/rgb/${DIRNAME_HASH}_${BASENAME}_%03d.png" &
       else
         echo ${FILENAME}, rgb single color, skipped
       fi
       if [ "${COLOR_TYPE}" == "rgba" ] || [ "${COLOR_TYPE}" == "srgba" ]; then
         if [ "$(convert "${FILENAME}" -alpha extract -format "%[k]" info:)" -gt "1" ]; then
-          echo ${FILENAME}, alpha \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
-          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png"
-          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png"
+          mkdir -p "${HR_OUTPUT_DIR}/${CATEGORY}/alpha"
+          mkdir -p "${LR_OUTPUT_DIR}/${CATEGORY}/alpha"
+          echo ${FILENAME}, alpha \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\) ${CATEGORY}
+          wait_for_jobs
+          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${CATEGORY}/alpha/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png" &
+          wait_for_jobs
+          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${CATEGORY}/alpha/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png" &
         else
           echo ${FILENAME}, alpha single color, skipped
         fi
@@ -77,5 +96,7 @@ find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \) | while read FILENA
   else
     echo ${FILENAME}, already processed, skipped
   fi
-
+  
 done
+
+wait_for_jobs
