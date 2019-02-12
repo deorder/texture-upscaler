f11a7ee836455cda7bb3502a5cee320891ba2776
Deorder <31899960+deorder@users.noreply.github.com>
2019-02-12 01:01:16 +0100

    Modified to create example and ground truth tiles

--- training/create_tiles.sh
+++ training/create_tiles.sh
@@ -3,18 +3,28 @@ ARGS="${@-./input}"
 
 shopt -s extglob
 
-SCALE_RATIO=1
-
 INPUT_DIR=${ARGS}
-OUTPUT_DIR=./output
 
-MIN_TILE_WIDTH=$((32 * ${SCALE_RATIO}))
-MIN_TILE_HEIGHT=$((32 * ${SCALE_RATIO}))
+MIN_TILE_WIDTH=64
+MIN_TILE_HEIGHT=64
 
-MAX_TILE_WIDTH=$((64 * ${SCALE_RATIO}))
-MAX_TILE_HEIGHT=$((64 * ${SCALE_RATIO}))
+MAX_TILE_WIDTH=128
+MAX_TILE_HEIGHT=128
 
-mkdir -p "${OUTPUT_DIR}"
+# Examples
+LR_SCALE=25%
+LR_FILTER=point
+LR_INTERPOLATE=Nearest
+LR_OUTPUT_DIR=./output.lr
+
+# Ground truth
+HR_SCALE=100%
+HR_FILTER=point
+HR_INTERPOLATE=Nearest
+HR_OUTPUT_DIR=./output.hr
+
+mkdir -p "${LR_OUTPUT_DIR}"
+mkdir -p "${HR_OUTPUT_DIR}"
 
 find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \) | while read FILENAME; do
 
@@ -45,14 +55,16 @@ find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \) | while read FILENA
 
       if [ "$(convert "${FILENAME}" -alpha off -format "%[k]" info:)" -gt "1" ]; then
         echo ${FILENAME}, rgb \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
-        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_%03d.png"
+        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_%03d.png"
+        convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_%03d.png"
       else
         echo ${FILENAME}, rgb single color, skipped
       fi
       if [ "${COLOR_TYPE}" == "rgba" ] || [ "${COLOR_TYPE}" == "srgba" ]; then
         if [ "$(convert "${FILENAME}" -alpha extract -format "%[k]" info:)" -gt "1" ]; then
           echo ${FILENAME}, alpha \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\)
-          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png"
+          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png"
+          convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME}_alpha_%03d.png"
         else
           echo ${FILENAME}, alpha single color, skipped
         fi
